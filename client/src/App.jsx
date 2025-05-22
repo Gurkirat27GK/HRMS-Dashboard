560932d29e5d0bf01df14ff99bbf8690974107dc
jatingrg <jatingarg94786@gmail.com>
2025-05-22 13:32:23 +0530

    adding

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <button className="bg-purple-700 text-white py-2 px-4 rounded-lg hover:bg-purple-800">
+  Register
+</button>
+
+    </>
+  )
+}
+
+export default App
